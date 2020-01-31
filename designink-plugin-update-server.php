39e1c02621fee45f8c5d90d7cc8863d4f0c03c30
Kyle Niemiec <kyle@designdigitalsolutions.com>
2020-01-31 14:28:30 -0700

    Added the plugin to the DesignInk plugin update system, removed the ID from the plugin headers.

--- designink-plugin-update-server.php
+++ designink-plugin-update-server.php
@@ -1,10 +1,9 @@
 <?php
 /**
  * Plugin Name: DesignInk Plugin Update Server
- * Plugin ID: di-update/plugin/designink-plugin-update-server
  * Plugin URI: https://designinkdigital.com/
  * Description: A proprietary WordPress solution provided by DesignInk Digital to manage your own, personal plugin repository, integrated with GitHub.
- * Version: 1.5.0
+ * Version: 1.5.1
  * Author: DesignInk Digital
  * Author URI: https://designinkdigital.com/
  * Text Domain: wporg
@@ -24,6 +23,7 @@
 defined( 'ABSPATH' ) or exit;
 
 use Designink\WordPress\Framework\v1_0_1\Plugin;
+use Designink\WordPress\Plugin_Update_Helper\v1_0_0\Plugin_Helper_Update_List;
 
 // Include DesignInk's framework
 require_once __DIR__ . '/vendor/designink/designink-wp-framework/index.php';
@@ -36,7 +36,16 @@ if ( ! class_exists( 'Designink_Plugin_Update_Server', false ) ) {
 	/**
 	 * The plugin wrapper class.
 	 */
-	final class Designink_Plugin_Update_Server extends Plugin { }
+	final class Designink_Plugin_Update_Server extends Plugin {
+
+		/**
+		 * Plugin entry point
+		 */
+		final public static function construct() {
+			Plugin_Helper_Update_List::add_plugin( 'designink-plugin-update-server', 'https://designinkdigital.com/' );
+		}
+
+	}
 
 	// Start it up.
 	Designink_Plugin_Update_Server::instance();
